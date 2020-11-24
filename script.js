cfa32e5f4bf716222e19eca2181908c1fe760367
Stephanie <stephanie.furlan94@gmail.com>
2020-11-24 17:26:42 +0100

    Added - move images on circles click

--- script.js
+++ script.js
@@ -7,13 +7,19 @@ $(document).ready(function() {
    $("#right").click(function() {
       nextImg();
    });
+
+   $("#buttons i").click(function() {
+      var index = $("#buttons i").index( this );
+      console.log(index);
+      moveToImg(index);
+   });
+
 });
 
 
 function prevImg() {
    // move to previous image
    var act = $(".active");
-   log(act);
    act.removeClass("active");
    // first-last check
    if (act.hasClass("first")) {
@@ -35,9 +41,18 @@ function nextImg() {
    }
 }
 
+function moveToImg(index) {
+   // remove all active classes
+   var act = $(".active");
+   act.removeClass("active");
+   // add active to img/i on index - index + 1
+   log($("#buttons i:nth-child(" + (index + 1) + ")").addClass("active"));
+   $("#images img:nth-child(" + (index + 1) + ")").addClass("active");
+}
 
 
 
+// log html
 var log = function($selector) {
     $selector.each(function() {
         console.log(this);
